de8af8feea5a79a0c9fa5acef9d3787b65b4a090
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-17 13:54:24 +0700

    menambahkan method set judul

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -5,5 +5,10 @@ using namespace std;
 
 class buku {
     string judul;
-} 
+
+public:
+    buku setJudul(string judul) {
+        this->judul = judul;
+        return *this; // chain function
+    }
 
