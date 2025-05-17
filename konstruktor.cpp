8eb39584a94cfbcfef99741d45cdb0845281d7ba
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-17 14:05:02 +0700

    Menambahkan overloading konstruktor untuk inisialisasi

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -6,4 +6,12 @@ class mahasiswa {
 private:
     int nim;
     string nama;
-}
+
+public:
+    mahasiswa();                            // Konstruktor default
+    mahasiswa(int);                         // Konstruktor dengan nim
+    mahasiswa(string);                      // Konstruktor dengan nama
+    mahasiswa(int iNim, string iNama);      // Konstruktor dengan nim dan nama
+    void cetak();
+};
+
